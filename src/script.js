0cbaa0af820667fe5d6cb5941d04c52937185099
kathink <121655715+kathink@users.noreply.github.com>
2023-01-30 17:59:48 +0700

    Updated from Codesandbox

--- src/script.js
+++ src/script.js
@@ -6,28 +6,29 @@ document.getElementById("search-btn").addEventListener("click", showSearchBar);
 
 function displayCurrentLocation() {
   function showTemp(response) {
-    let city = document.querySelector("#city");
     let currentLocation = response.data.name;
     let currentDegree = Math.round(response.data.main.temp);
-    let changeDegree = document.querySelector("#current-degree");
-    city.innerHTML = currentLocation;
-    changeDegree.innerHTML = currentDegree;
+    document.querySelector("#current-degree").innerHTML = currentDegree;
+    document.querySelector("#city").innerHTML = currentLocation;
   }
   function describeWeather(response) {
     let currentHour = currentDate.getHours();
     let hourlyHumidity = response.data.hourly[currentHour].humidity;
-    let humidity = document.querySelector("#humidity-percentage");
-    humidity.innerHTML = `${hourlyHumidity}%`;
     let hourlyPrecipitation = Math.round(
       response.data.hourly[currentHour].pop * 100
     );
-    let precipitation = document.querySelector("#precipitation-percentage");
-    precipitation.innerHTML = `${hourlyPrecipitation}%`;
     let hourlyWinSpeed = Math.round(
       response.data.hourly[currentHour].wind_speed * 3.6
     );
-    let winSpeed = document.querySelector("#win-speed-kilometer-per-hour");
-    winSpeed.innerHTML = `${hourlyWinSpeed}km/h`;
+    document.querySelector(
+      "#humidity-percentage"
+    ).innerHTML = `${hourlyHumidity}%`;
+    document.querySelector(
+      "#precipitation-percentage"
+    ).innerHTML = `${hourlyPrecipitation}%`;
+    document.querySelector(
+      "#win-speed-kilometer-per-hour"
+    ).innerHTML = `${hourlyWinSpeed}km/h`;
     let hourlyUvi = response.data.hourly[currentHour].uvi;
     let uvIndex = document.querySelector("#uv-index");
     if (hourlyUvi < 2) {
@@ -54,59 +55,65 @@ function displayCurrentLocation() {
   }
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
-let popup = document.querySelector(".current-location-icon");
-popup.addEventListener("click", displayCurrentLocation, true);
+document
+  .querySelector(".current-location-icon")
+  .addEventListener("click", displayCurrentLocation, true);
 
-function searchCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
+function describeWeather(response) {
+  let currentHour = currentDate.getHours();
+  let hourlyHumidity = response.data.hourly[currentHour].humidity;
+  let hourlyPrecipitation = Math.round(
+    response.data.hourly[currentHour].pop * 100
+  );
+  document.querySelector(
+    "#humidity-percentage"
+  ).innerHTML = `${hourlyHumidity}%`;
+  document.querySelector(
+    "#precipitation-percentage"
+  ).innerHTML = `${hourlyPrecipitation}%`;
+  let hourlyWinSpeed = Math.round(
+    response.data.hourly[currentHour].wind_speed * 3.6
+  );
+  document.querySelector(
+    "#win-speed-kilometer-per-hour"
+  ).innerHTML = `${hourlyWinSpeed}km/h`;
+  let hourlyUvi = response.data.hourly[currentHour].uvi;
+  let uvIndex = document.querySelector("#uv-index");
+  if (hourlyUvi < 2) {
+    uvIndex.innerHTML = "Low";
+  } else if (3 < hourlyUvi < 5) {
+    uvIndex.innerHTML = "Moderate";
+  } else if (6 < hourlyUvi < 7) {
+    uvIndex.innerHTML = "High";
+  } else if (8 < hourlyUvi < 10) {
+    uvIndex.innerHTML = "Very High";
+  } else {
+    uvIndex.innerHTML = "Extreme";
+  }
+}
+function showTemp(response) {
+  let currentDegree = Math.round(response.data.main.temp);
+  document.querySelector("#current-degree").innerHTML = currentDegree;
   let city = document.querySelector("#city");
-  function describeWeather(response) {
-    let currentHour = currentDate.getHours();
-    let hourlyHumidity = response.data.hourly[currentHour].humidity;
-    let humidity = document.querySelector("#humidity-percentage");
-    humidity.innerHTML = `${hourlyHumidity}%`;
-    let hourlyPrecipitation = Math.round(
-      response.data.hourly[currentHour].pop * 100
-    );
-    let precipitation = document.querySelector("#precipitation-percentage");
-    precipitation.innerHTML = `${hourlyPrecipitation}%`;
-    let hourlyWinSpeed = Math.round(
-      response.data.hourly[currentHour].wind_speed * 3.6
-    );
-    let winSpeed = document.querySelector("#win-speed-kilometer-per-hour");
-    winSpeed.innerHTML = `${hourlyWinSpeed}km/h`;
-    let hourlyUvi = response.data.hourly[currentHour].uvi;
-    let uvIndex = document.querySelector("#uv-index");
-    if (hourlyUvi < 2) {
-      uvIndex.innerHTML = "Low";
-    } else if (3 < hourlyUvi < 5) {
-      uvIndex.innerHTML = "Moderate";
-    } else if (6 < hourlyUvi < 7) {
-      uvIndex.innerHTML = "High";
-    } else if (8 < hourlyUvi < 10) {
-      uvIndex.innerHTML = "Very High";
-    } else {
-      uvIndex.innerHTML = "Extreme";
-    }
-  }
-  function showTemp(response) {
-    let currentDegree = Math.round(response.data.main.temp);
-    let changeDegree = document.querySelector("#current-degree");
-    changeDegree.innerHTML = currentDegree;
-    city.innerHTML = response.data.name;
-    let cityLatitude = response.data.coord.lat;
-    let cityLongitude = response.data.coord.lon;
-    let hourlyApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&appid=${apiKey}`;
-    axios.get(hourlyApi).then(describeWeather);
-  }
+  city.innerHTML = response.data.name;
+  let cityLatitude = response.data.coord.lat;
+  let cityLongitude = response.data.coord.lon;
+  let apiKey = `f5029b784306910c19746e40c14d6cd3`;
+  let hourlyApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&appid=${apiKey}`;
+  axios.get(hourlyApi).then(describeWeather);
+}
+function searchCity(city) {
   let apiKey = `f5029b784306910c19746e40c14d6cd3`;
   let units = `metric`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemp);
 }
-let submitButton = document.querySelector("#search-form");
-submitButton.addEventListener("submit", searchCity);
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input").value;
+  searchCity(searchInput);
+}
+document.querySelector("#search-form").addEventListener("submit", handleSubmit);
 
 function formatDate(date) {
   let days = [
@@ -124,5 +131,5 @@ function formatDate(date) {
   return `${currentDay} ${currentHour}:${currentMinutes}`;
 }
 let currentDate = new Date();
-let dateTime = document.querySelector("#date-time");
-dateTime.innerHTML = formatDate(currentDate);
+document.querySelector("#date-time").innerHTML = formatDate(currentDate);
+searchCity("London");
